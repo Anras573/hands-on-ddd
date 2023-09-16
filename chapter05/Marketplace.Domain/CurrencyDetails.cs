944d06c50dc45011c5b9ee9c1364299734240a29
Anders Bo Rasmussen <anras573@gmail.com>
2023-09-16 19:34:02 +0200

    Mark Properties in CurrencyDetails.cs as Init Only.

--- chapter05/Marketplace.Domain/CurrencyDetails.cs
+++ chapter05/Marketplace.Domain/CurrencyDetails.cs
@@ -2,9 +2,9 @@
 
 public record CurrencyDetails
 {
-    public string? CurrencyCode { get; set; }
-    public bool InUse { get; set; }
-    public int DecimalPlaces { get; set; }
+    public string? CurrencyCode { get; init; }
+    public bool InUse { get; init; }
+    public int DecimalPlaces { get; init; }
 
     public static CurrencyDetails None => new () { InUse = false };
 }
